81e824e9ac42093af9b9ce7b2af9d964ba58f140
Kaustav Majumder <kmajumde@redhat.com>
2019-04-12 19:59:23 +0530

    Removed old files

--- prepare-test-database.sh
+++
@@ -1,6 +0,0 @@
-LOG_FILE=/var/log/pg-bench-results/prepare-test-db.log
-su - postgres -c "createdb pgbench"
-
-su - postgres -c "time pgbench -i -s 4000 pgbench" >> $LOG_FILE 2>&1
-
-echo "Done setting databse"
