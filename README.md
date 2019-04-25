b35ae7d62a5a2ad83e509393500eb5e0aa82be97
Kaustav Majumder <kmajumde@redhat.com>
2019-04-25 15:07:59 +0530

    Added readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # pg-bench-test-automation
+
+**A  benchmarking tool for [ovirt](https://github.com/ovirt) storage environments.**
+
+***Prerequisites***
+
++ ovirt engine sdk
++ psycopg2
++ Paswordless ssh connection between the hosts
++ edit hosts.txt
++ Ovirt Api url
++ Edit connection url and creds in ./presetup/connection.py
+
+*Optional*
+
++ Fork this [wilfriedroset/pgbencher](https://github.com/wilfriedroset/pgbencher)
+and edit $PGBENCHER_ROOT/defaults/main.yml > `bench_plan`
++ Push changes to your git repository
++ Edit ./post_setup/settup_pg_bencher.sh > git clone <Your repo here>
+
+**To run test**
+
+```
+⌁ [pg_bench_test_automation] master+ ± bash start.sh
+```
+
+
+The results will be stored in a folder inside $PROJECT_HOME as (results-master-*)
--- common/run_iostat_vmstat_sar.sh
+++ common/run_iostat_vmstat_sar.sh
@@ -2,13 +2,13 @@
 
 for host in "$@"; do {
 echo $host
- ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${host} -t 'cat <<EOF > /etc/profile.d/pgbench-test_env.sh
+ ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${host} -tt 'cat <<EOF > /etc/profile.d/pgbench-test_env.sh
  export GLUSTER_PROFILER_RESULTS=/home/gluster-profiler-$HOSTNAME-$(date +%Y%m%d_%H%M%S)
  export MON_INTERVAL=10'
 
- ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${host} -t 'mkdir -p $GLUSTER_PROFILER_RESULTS'
+ ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${host} -tt 'mkdir -p $GLUSTER_PROFILER_RESULTS'
 
- ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${host} -t " top -bd \$MON_INTERVAL > \$GLUSTER_PROFILER_RESULTS/top_proc.\$HOSTNAME.txt &
+ ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${host} -tt " top -bd \$MON_INTERVAL > \$GLUSTER_PROFILER_RESULTS/top_proc.\$HOSTNAME.txt &
    top -bHd \$MON_INTERVAL > \$GLUSTER_PROFILER_RESULTS/top_thr.\$HOSTNAME.txt &
    iostat -Ntkdx \$MON_INTERVAL >\$GLUSTER_PROFILER_RESULTS/iostat.\$HOSTNAME.txt &
    vmstat -t \$MON_INTERVAL > \$GLUSTER_PROFILER_RESULTS/vmstat.\$HOSTNAME.txt &
